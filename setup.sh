4c476bc729b4d389fb5983f44c7b7f8b6cb10438
Peter <peter.kaminski09@gmail.com>
2017-10-18 21:39:17 +0000

    Updated the setup script to add a cron job

--- setup.sh
+++ setup.sh
@@ -10,5 +10,10 @@ wget https://github.com/Eelviny/nxt-python/archive/v2.2.2.tar.gz
 tar xzf nxt-python-2.2.2.tar.gz
 cd nxt-python-2.2.2
 python setup.py install
+cd
+mkdir automated-pylog
+sudo crontab -l > mycron
+echo "@reboot sh /home/pi/nxt-pythonpi-client/start.sh >/home/pi/automated-pylog/cronlog 2>&1" >> mycron
+sudo crontab mycron
 echo "Restart required...restarting now"
 shutdown -r
